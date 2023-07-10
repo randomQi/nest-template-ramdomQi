943f6f8fe7e54108f9a8cbc812a6b3d51f550c73
lq <1781373476@qq.com>
2023-07-10 14:06:39 +0800

    init tempalte

---
+++ src/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UserService {
+  async create(createUserDto: CreateUserDto): Promise<any> {
+    return {
+      code: 200,
+      message: '请求成功',
+      data: Array.from({ length: 50 }, (name, index) => `测试数据${index}`),
+    };
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
