901307a5a6aaa469043da738f06f90509e4b2351
lq <1781373476@qq.com>
2023-07-18 09:46:35 +0800

    feat： 自定义全局异常过滤器

---
+++ src/filters/httpexception/httpexception.filter.ts
@@ -0,0 +1,21 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const httpArgumentsHost = host.switchToHttp();
+    // 获取请求与响应对象
+    const request = httpArgumentsHost.getRequest();
+    const response = httpArgumentsHost.getResponse();
+    // 获取异常状态码
+    const status = exception.getStatus();
+
+    // 定制统一异常响应数据结构体
+    response.json({
+      title: '统一制定的httpException异常响应的数据结构体',
+      code: status,
+      timestamp: new Date().toDateString(),
+      message: exception.message || exception.name
+    })
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from "./filters/httpexception/httpexception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000,'0.0.0.0');
+  app.useGlobalFilters(new HttpExceptionFilter)
   let httpServer =  await app.getUrl();
   console.log(httpServer);
 }
